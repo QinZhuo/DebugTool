9bf247c7299e14c25d11812254ac1e483c9483d2
琴卓 <1043665802@qq.com>
2019-09-02 02:55:25 +0800

    初始化

---
+++ Assets/Test/Script/DebugTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogWarning("warningTets");
+        Debug.Log("warningTets1");
+        Debug.LogError("warningTets2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("logtest");
+        Debug.LogError("logtest" + (int)(Time.time) % 1000000);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.LogError("errorTest A"+Time.time);
+        }
+    }
+}
